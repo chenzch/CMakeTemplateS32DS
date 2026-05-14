08ee39d8d013d9d4e73fd30044d5d5cafb0334e5
Peter Chen <chenwilsoft@msn.com>
2026-05-14 16:47:09 +0800

    build(cmake): 添加OUTPUT_STRIP_TRAILING_WHITESPACE清理gcc插件目录输出
    为gcc14工具链的execute_process调用添加输出尾空白清理，避免多余空格污染GCCPLUGINS_DIR变量值

--- cmake/toolchain/gcc14.cmake
+++ cmake/toolchain/gcc14.cmake
@@ -5,6 +5,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi.cmake)
 execute_process(
     COMMAND ${CMAKE_C_COMPILER} -print-file-name=plugin
     OUTPUT_VARIABLE GCCPLUGINS_DIR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VER)
